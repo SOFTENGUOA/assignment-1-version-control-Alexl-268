0bf3ded52753cf2b6d6beb98ccaab470ca03f3fa
hackerman268 <zlia715@aucklanduni.ac.nz>
2021-08-05 17:00:00 +1200

    Implemented tests for Task 2
    Increment failed because
    Increment failed because

--- src/se283/a1/t2/CounterTest.java
+++ src/se283/a1/t2/CounterTest.java
@@ -6,6 +6,7 @@ package se283.a1.t2;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import se283.a1.t1.Counter;
 
 /**
  * Test class for Counter class
@@ -20,7 +21,11 @@ class CounterTest {
 	 */
 	@Test
 	void testIncrement() {
-		fail("Not yet implemented");
+		//fail("Not yet implemented");
+		Counter counter = new Counter();
+		int before = counter._ctr ;
+		counter.increment();
+		assertEquals(counter._ctr,before+1);
 	}
 
 	/**
@@ -28,15 +33,23 @@ class CounterTest {
 	 */
 	@Test
 	void testDecrement() {
-		fail("Not yet implemented");
+//		fail("Not yet implemented");
+		Counter counter = new Counter();
+		int before = counter._ctr;
+		counter.decrement();
+		assertEquals(counter._ctr,before-1);
 	}
+
 
 	/**
 	 * Test method for {@link se283.a1.t1.Counter#reset()}.
 	 */
 	@Test
 	void testReset() {
-		fail("Not yet implemented");
+//		fail("Not yet implemented");
+		Counter counter = new Counter();
+		counter.reset();
+		assertEquals(counter._ctr, 0);
 	}
 
 	/**
@@ -44,7 +57,12 @@ class CounterTest {
 	 */
 	@Test
 	void testMultiplyBy() {
-		fail("Not yet implemented");
+//		fail("Not yet implemented");
+		Counter counter = new Counter();
+		int before = counter._ctr;
+		counter.multiplyBy(2);
+		assertEquals(counter._ctr,before*2);
 	}
+
 
 }
